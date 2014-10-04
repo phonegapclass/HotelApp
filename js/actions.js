04871e1bfbb27c402f62ed0f45c2a1e21351f764
Carlos Celma <solrac@igitsoft.com>
2014-10-04 12:51:44 -0500

    cvb

--- js/actions.js
+++ js/actions.js
@@ -1,23 +1,28 @@
 //actions.js
 var fn = {
     init: function(){
+        //document.addEventListener("deviceready", yourCallbackFunction, false); //phonga
         document.addEventListener('deviceready',fn.device,false);
+        //$('#regSend').click(); //Producir un click
     },
     device: function(){
-        var x = false;
-        if(!x)
-            window.location.href = '#reg';
-        $('#regSend').click(fn.registro);
+      var x = false;
+      if(!x)
+        window.location.href =  '#reg';
+      $('#regSend').click(fn.registro);
     },
     registro: function(){
         var nombre = $('#regName').val();
         var mail = $('#regMail').val();
-        var tel = $('#regTel').val();
+        var tel = $('#regTel').val(); 
         if(nombre != '' && mail != '' && tel != ''){
-            //Enviar Datos al Servidor
+          //Enviar datos al Servidor.
+            
         }else{
-            navigator.notification.alert("Todos los campos son requeridos",null,'hola','bye');
+          alert("Todos los campos son requeridos.");
         }
+        
+        
     }
 };
 $(fn.init);
