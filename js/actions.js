2de1a0e79546d7811d6df72eb8b104f25ff632fd
Carlos Celma <solrac@igitsoft.com>
2014-10-11 11:20:49 -0500

    asd

--- js/actions.js
+++ js/actions.js
@@ -7,21 +7,32 @@ var fn = {
     },
     device: function(){
         var x = false;
-            if(!x)
-            window.location.href =  '#reg';
+        if(!x)
+            window.location.href = '#reg';
         $('#regTake').tap(myCapture.tomarFoto);
         $('#regSend').tap(fn.registro);
     },
     registro: function(){
         var nombre = $('#regName').val();
         var mail = $('#regMail').val();
-        var tel = $('#regTel').val(); 
-        if(nombre != '' && mail != '' && tel != ''){
+        var tel = $('#regTel').val();
+        var foto = $('#regTake').attr('rel');
+        if(nombre != '' && mail != '' && tel != '' && foto != '' && foto != undefined){
             //Enviar datos al Servidor.
-            navigator.notification.beep(2);
+            fn.enviarRegistro(nombre, mail, tel, foto);
         }else{
             navigator.notification.alert("Todos los campos son requeridos.",null,'Error','Aceptar');
         } 
+    },
+    enviarRegistro: function(nom, mail, tel, foto){
+        $.ajax({
+            type: "POST",
+            url: "http://carlos.igitsoft.com/apps/test.php",
+            data: {nom:nom,mail:mail,tel:tel}
+        }).done(function(respuesta) {
+            if( respuesta == '1' )
+                myTransfer.ft.upload(foto, "http://carlos.igitsoft.com/apps/test.php", myTransfer.subido,myTransfer.error,myTransfer.opciones,true);
+        });
     }
 };
 $(fn.init);
