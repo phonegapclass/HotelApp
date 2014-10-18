157fe5329b639dd44a3ddfc57ae6132a0aeb9f07
Carlos Celma <solrac@igitsoft.com>
2014-10-18 13:17:35 -0500

    asd

--- js/API/webSQL.js
+++ js/API/webSQL.js
@@ -3,6 +3,7 @@ var db = {
         var obj = window.openDatabase("hotel", "1.0", "Hotel Demo", 200000);
         return obj;
     },
+    //--------------------PENDIENTES------------------
     agregarPendientes: function(th,ha,pr,di){
         db.th = th;
         db.ha = ha;
@@ -15,9 +16,27 @@ var db = {
         tx.executeSql("INSERT INTO pendientes (th, ha, pr, di) VALUES ('" + db.th + "', '" + db.ha + "', '" + db.pr + "', '" + db.di + "')");
     },
     exitoPendientes: function(){
+        $.mobile.loading( 'hide' );
+        navigator.notification.alert('Reserva en espera de conexión',null,'Guardado','Aceptar');
+    },
+    //-------------------HISTORIAL---------------------
+    agregarHistorial: function(th,ha,pr,di){
+        db.th = th;
+        db.ha = ha;
+        db.pr = pr;
+        db.di = di;
+        db.crearDB().transaction(db.tablaHistorial,db.error,db.exitoHistorial);
+    },
+    tablaHistorial: function(tx){
+        tx.executeSql("CREATE TABLE IF NOT EXISTS historial (id unique, th, ha, pr, di)");
+        tx.executeSql("INSERT INTO historial (th, ha, pr, di) VALUES ('" + db.th + "', '" + db.ha + "', '" + db.pr + "', '" + db.di + "')");
+    },
+    exitoHistorial: function(){
+        $.mobile.loading( 'hide' );
         navigator.notification.alert('Reserva en espera de conexión',null,'Guardado','Aceptar');
     },
     error: function(err){
+        $.mobile.loading( 'hide' );
         alert('Error: '+err.code);
     }
 }
--- js/actions.js
+++ js/actions.js
@@ -42,6 +42,18 @@ var fn = {
             }
         });
     },
+    enviarReserva: function(th, ha, pr, di){
+        $.mobile.loading( 'show' );
+        $.ajax({
+            type: "POST",
+            url: "http://carlos.igitsoft.com/apps/test.php",
+            data: {tipo:th,habitaciones:ha,personas:pr,dias:di}
+        }).done(function(respuesta){
+            if( respuesta == '1' ){
+                db.agregarHistorial(th,ha,pr,di);
+            }
+        });
+    },
     storage: window.localStorage,
     estaRegistrado: function(){
         if(fn.storage.getItem('registro') == 1)
@@ -72,9 +84,10 @@ var fn = {
         if(th != '' && ha != '' && pr != '' && di != ''){
             if(connection.estaConectado()){
                 //Enviar Reserva a servidor
-                alert();
+                fn.enviarReserva(th,ha,pr,di);
             }else{
                 //Guardar los datos hasta conexión
+                $.mobile.loading( 'show' );
                 db.agregarPendientes(th,ha,pr,di);
             }
         }else{
