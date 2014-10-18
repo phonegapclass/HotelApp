bcbb9014e5e41231ce6e6513c175c3fb47f72783
Carlos Celma <solrac@igitsoft.com>
2014-10-18 12:35:41 -0500

    asd

--- js/API/webSQL.js
+++ js/API/webSQL.js
@@ -9,7 +9,7 @@ var db = {
         db.ha = ha;
         db.pr = pr;
         db.di = di;
-        db.crearDB.transaction(db.tablaPendientes,db.error,db.exitoPendientes);
+        db.crearDB().transaction(db.tablaPendientes,db.error,db.exitoPendientes);
     },
     tablaPendientes: function(tx){
         alert(db.th);
